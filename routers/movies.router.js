bff0ea6913ab4e34f1fcc88b520e61c358ce9fbf
Osmar Guerra <criosos28@gmail.com>
2023-11-30 18:49:07 -0700

    V1.01

---
+++ routers/movies.router.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/movies.controller');
+
+router.get('/', moviesController.getMovies);
+
+router.get('/:movieId', moviesController.getMovieById);
+
+router.post('/', moviesController.newMovies);
+
+router.put('/:movieId',moviesController.updateMovie);
+
+router.delete('/:movieId', moviesController.deleteMovie);
+
+module.exports = router;
